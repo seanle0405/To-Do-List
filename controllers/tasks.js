6101c8ab09c7eb6336f2bbe3a7fabb1b3d517b7f
Sean <seanle0405@hotmail.com>
2019-06-06 17:05:08 -0700

    added retaled to property

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -7,7 +7,7 @@ const Task = require("../models/tasks.js")
 //INDEX
 router.get("/", (req,res) =>{
 	Task.find({}, (err, allTasks)=>{
-		res.render("index.ejs", {hTasks: allTasks.filter(task => task.priority === "high" && !task.completed), mTasks: allTasks.filter(task => task.priority === "medium" && !task.completed), lTasks: allTasks.filter(task => task.priority === "low" && !task.completed)})
+		res.render("index.ejs", {hTasks: allTasks.filter(task => task.priority === "high" && !task.completed), mTasks: allTasks.filter(task => task.priority === "medium" && !task.completed), lTasks: allTasks.filter(task => task.priority === "low" && !task.completed), cTasks: allTasks.filter(task => task.completed)})
 	})
 })
 
@@ -16,6 +16,11 @@ router.get("/new", (req,res)=>{
 	res.render("new.ejs")
 })
 router.post("/", (req,res)=>{
+	let relatedTo = req.body.relatedTo.split(",")
+	for(let i = 0; i< relatedTo.length; i++){
+		relatedTo[i] = relatedTo[i].trim()
+	}
+	req.body.relatedTo = relatedTo
 	Task.create(req.body, (err, createdTask)=>{
 		console.log(createdTask)
 		res.redirect("/tasks")
@@ -50,8 +55,18 @@ router.put("/:id", (req, res)=>{
 	}else{
 		req.body.completed = false
 	}
-	Task.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel)=>{
+	Task.findByIdAndUpdate(req.params.id, req.body,  (err, updatedModel)=>{
 		res.redirect("/tasks")
+	})
+})
+
+//COMPLETED & UNDO
+router.post("/:id", (req, res) =>{
+	Task.findById(req.params.id, (err, foundTask) =>{
+		foundTask.completed = !foundTask.completed
+		foundTask.save((err, updatedTask)=>{
+			res.redirect("/tasks")
+		})
 	})
 })
 
