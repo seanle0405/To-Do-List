6331fe068baf356e27aab32b6396623594466f5e
Sean Le <seanle0405@hotmail.com>
2019-06-08 12:39:03 -0700

    changed default path

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override")
 const tasksController = require("./controllers/tasks.js")
 const usersController = require("./controllers/users.js")
 const sessionsController = require("./controllers/sessions.js")
+const Task = require("./models/tasks.js")
 const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/basiccrud'
 
@@ -23,6 +24,16 @@ app.use(session({
 mongoose.connect(MONGODB_URI, {userNewUrlParser: true})
 mongoose.connection.once("open", ()=>{
 	console.log("connected to mongo")
+})
+
+app.get("/getalltasks", (req,res) =>{
+	Task.find({}, (err, allTasks)=>{
+		res.send(allTasks)
+	})
+})
+
+app.get("/", (req,res) =>{
+	res.redirect("/tasks")
 })
 
 app.use("/sessions", sessionsController)
